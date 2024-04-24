0e02f3ac526fe93eaff35f507ead85911d509a1d
kgorski1 <01187228@pw.edu.pl>
2024-04-24 13:50:47 +0200

    added main func with ascii shades

---
+++ mainApp.py
@@ -0,0 +1,5 @@
+from converter import Converter
+
+if __name__ == "__main__":
+    asciiShades = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1\\{\\}[]?-_+~<>i!lI;:,"^`\'."'
+    Converter(asciiShades)
