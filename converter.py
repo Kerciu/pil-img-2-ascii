5f2f9d3d14144a1acefa411400b4829c1894e468
kgorski1 <01187228@pw.edu.pl>
2024-04-24 13:21:46 +0200

    added image property saving func

--- converter.py
+++ converter.py
@@ -2,3 +2,25 @@
 # TODO TRANSFORM TO THE SHADES OF GREY
 # TODO MAP PIXELS TO ASCII
 # TODO RENDER TEXT FILE
+
+import os, sys
+from PIL import Image
+
+
+def openImage(fileHandle: str) -> Image:
+    try:
+        imageData = {}
+        with Image.open(fileHandle) as im:
+            imageData["height"] = im.height
+            imageData["mode"] = im.mode
+            imageData["width"] = im.width
+            imageData["colors"] = im.getcolors()
+            imageData["histogram"] = im.histogram()
+            imageData["format"] = im.format
+
+    except FileNotFoundError:
+        raise FileNotFoundError("File was not found")
+    except Exception as e:
+        raise Exception("There was an error while processing your image: ", e)
+
+    return imageData
