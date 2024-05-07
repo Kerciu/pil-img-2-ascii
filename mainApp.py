008186abe9b6a8a9b78069bef19afa88b43f7a0a
Kerciu <kerciuuu@gmail.com>
2024-05-07 20:05:22 +0200

    added argparser

--- mainApp.py
+++ mainApp.py
@@ -1,16 +1,28 @@
 from converter import Converter
+from argparse import ArgumentParser
+
+
+def parseArgs():
+    parser = ArgumentParser()
+    parser.add_argument("--file-name", required=True)
+    parser.add_argument("--cols", type=int, default=80, help="Determine number of collumns in ASCII art")
+    args = parser.parse_args()
+    return args
+
 
 if __name__ == "__main__":
     """
     https://paulbourke.net/dataformats/asciiart/
     """
-    image = "C:\\Users\\Kacper\\Desktop\\Folder\\Profilowe.png"
+    args = parseArgs()
     asciiShades = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1\\{\\}[]?-_+~<>i!lI;:,"^`\'."'
     convert = Converter(asciiShades)
 
     newWidth = 100
     newHeight = 100
 
-    output_file = "KolosAndrzeja.txt"
+    output_file = "ptoszek.txt"
 
-    convert.scaleAndConvertToASCII(image, output_file, newWidth, newHeight)
+    imageObj = convert.openImage(args.file_name)
+    txt = convert.convertToASCII(imageObj, 0.42, args.cols)
+    convert.renderTextFile(txt)
